c1efccd1a03414dcd7a1f89fa6a4e0fcf2c132bc
Vandanachandramohan <vrvandana97@gmail.com>
2021-11-13 22:12:00 +0530

    new changes

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 let email = document.getElementById("inputEmail4");
 let pwd = document.getElementById("inputPassword4");
-let pwd1=document.getElementById("inputPassword");
-let phone=document.getElementById("inputPhone");
-let error= document.getElementById("error");
-let error1= document.getElementById("error1");
-let error2=document.getElementById("error2");
-let error3=document.getElementById("error3");
-
-/*function validation(){
-
-    if(email.value.trim()=="" || pwd.value.trim()==""){
-    alert("fields cannot be empty");
-      return false;
-    }
-
-    else {
-        //alert("validation is proper");
-        return true;
-    }
-}*/
+let pwd1 = document.getElementById("inputPassword");
+let phone = document.getElementById("inputPhone");
+let error = document.getElementById("error");
+let error1 = document.getElementById("error1");
+let error2 = document.getElementById("error2");
+let error3 = document.getElementById("error3");
 
 /* email-validation */
     
-    function validate(){
+    function validate() {
 
     let regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2,3})(.[a-z]{2,3})?$/
     if(regexp.test(email.value)){
@@ -39,9 +26,9 @@ let error3=document.getElementById("error3");
 
 /* pwd-validation */
 
-function validate1(){
-    let regex1= /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-    if(regex1.test(pwd.value)){
+function validate1() {
+    let regexp1= /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+    if(regexp1.test(pwd.value)){
         error1.innerHTML="Valid";
         error1.style.color="green";
         
@@ -69,35 +56,16 @@ function validate1(){
 
 function validate2() {
 
-let regex2 = /^\d{10}$/;
+let regexp2 = /^\d{10}$/;
 
- if(regex2.test(phone.value)){
-     error3.innerHTML="Valid phoneno";
-     error3.style.color="green";
-     return true;
-    }
-    else{
-        error3.innerHTML="Invalid phoneno";
-        error3.style.color="red";
-        return false;
-    }
+if(regexp2.test(phone.value)){
+    error3.innerHTML="Valid format";
+    error3.style.color="green";
+    return true;
 }
-
-function validate3() {
-
-let regex3 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-
-    if(phone.value.match(regex3)){
-        error3.innerHTML="Valid phone.no";
-        error3.style.color="green";
-        return true;
-        }
-     
-        else{
-            error3.innerHTML="Wrong format";
-            error3.style.color="red";
-            return false;
-        }
-
-
+else{
+    error3.innerHTML="Invalid format";
+    error3.style.color="red";
+    return false;
+    }
 }
